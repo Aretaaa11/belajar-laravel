b7d7e85fec5534924049f5ebe82a133d5425b7cc
Siti Syahda Rana Areta <reta.syahda@nawatech.co>
2025-11-22 15:47:23 +0700

    feat: Add admin dashboard with Blade templating).

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -1,31 +1,67 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-<div class="container">
-<a class="navbar-brand fw-bold text-primary" href="#">TokoKu</a>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-
-target="#navbarNav">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
-<ul class="navbar-nav ms-auto">
-<li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
-<li class="nav-item"><a class="nav-link" href="">Produk</a></li>
-{{-- Muncul hanya kalau sudah login --}}
-@auth
-<li class="nav-item"><a class="nav-link" href="">Pesanan</a></li>
-<li class="nav-item"><a class="nav-link" href="">Pembayaran</a></li>
-@endauth
-</ul>
-{{-- Kalau belum login tampilkan tombol Login --}}
-@guest
-@endguest
-<a href="{{ route('login') }}" class="btn btn-primary ms-lg-3">Login</a>
-{{-- Kalau sudah login tampilkan tombol Logout --}}
-@auth
-@csrf
-</form>
-@endauth
-<form method="POST" action="{{ route('logout') }}" class="d-inline">
-<button type="submit" class="btn btn-danger ms-lg-3">Logout</button>
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-
+items-center bg-navbar-theme"
+id="layout-navbar">
+<div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-
+none">
+<a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+<i class="bx bx-menu bx-sm"></i>
+</a>
 </div>
+<div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+<ul class="navbar-nav flex-row align-items-center ms-auto">
+<!-- User -->
+<li class="nav-item navbar-dropdown dropdown-user dropdown">
+<a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-
+bs-toggle="dropdown">
+<div class="avatar avatar-online">
+<img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-
+circle" />
+</div>
+</a>
+<ul class="dropdown-menu dropdown-menu-end">
+<li>
+  <a class="dropdown-item" href="#">
+<div class="d-flex">
+<div class="flex-shrink-0 me-3">
+<div class="avatar avatar-online">
+<img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto
+rounded-circle" />
+</div>
+</div>
+<div class="flex-grow-1">
+<span class="fw-semibold d-block">John Doe</span>
+<small class="text-muted">Admin</small>
+</div>
+</div>
+</a>
+</li>
+<li>
+<div class="dropdown-divider"></div>
+</li>
+<li>
+<a class="dropdown-item" href="#">
+<i class="bx bx-user me-2"></i>
+<span class="align-middle">My Profile</span>
+</a>
+</li>
+<li>
+<a class="dropdown-item" href="#">
+<i class="bx bx-cog me-2"></i>
+<span class="align-middle">Settings</span>
+</a>
+</li>
+<li>
+<div class="dropdown-divider"></div>
+</li>
+<li>
+<a class="dropdown-item" href="auth-login-basic.html">
+<i class="bx bx-power-off me-2"></i>
+<span class="align-middle">Log Out</span>
+</a>
+</li>
+</ul>
+</li>
+<!--/ User -->
+</ul>
 </div>
 </nav>
